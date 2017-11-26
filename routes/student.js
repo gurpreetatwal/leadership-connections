abda893d0c5b53dc963401a14643f8c37d01ce7a
Gurpreet Atwal <git@gatwal.com>
2017-11-25 16:26:51 -0800

    pull students from database instead of file

--- routes/student.js
+++ routes/student.js
@@ -1,16 +1,32 @@
 'use strict';
 
+const _ = require('lodash');
 const Router = require('koa-router');
-const fs = require('fs');
-const path = require('path');
+const parser = require('koa-body');
+
+const knex = require('../lib/knex');
 
 const router = new Router();
 
+const form = parser({json: false, urlencoded: false, multipart: true});
+
 router.get('/', async (ctx, next) => {
   await next();
-  // TODO this should fetch from a database
+  const students = await knex('student').select();
+  students.forEach(s => s.name = _.startCase(s.name));
   ctx.response.type = 'application/json';
-  ctx.response.body = fs.createReadStream(path.join(__dirname, '..', 'seeds', 'students.json'));
+  ctx.response.body = students;
+});
+
+router.post('/', form, async (ctx, next) => {
+  await next();
+
+  const student = ctx.request.body;
+  ctx.assert(student.name, 400, 'Each student must have a name');
+  ctx.assert(student.image, 400, 'Each student must have an image');
+  student.name = student.name.toLowerCase();
+
+  // TODO allow for adding students from UI
 });
 
 module.exports = router;
