5327db5f4595f843f0ba67d76bc47c53a794c4e7
Gurpreet Atwal <git@gatwal.com>
2017-11-25 18:05:50 -0800

    modify pairing router to actually generate pairs

--- routes/pairing.js
+++ routes/pairing.js
@@ -2,13 +2,32 @@
 
 const _ = require('lodash');
 const Router = require('koa-router');
+const moment = require('moment');
 const parser = require('koa-body');
 
 const knex = require('../lib/knex');
 
 const router = new Router();
-
 const url = {urlencoded: true, multipart: false, json: false};
+
+// TODO these should not be hardcoded, but should be part of class data
+const start = moment('2017-08-24');
+const weeks = 15;
+const classId = 'd0257fa9-ebb3-4c25-b880-e13740b2334c';
+
+async function attachStudents(pairings) {
+
+
+  // TODO ew, this needs to be parallel
+  for (let i = 0; i < pairings.length; i++) {
+    const pair = pairings[i];
+    pair.student_1 = (await knex.select().from('student').where('id', pair.student_1))[0];
+    pair.student_2 = (await knex.select().from('student').where('id', pair.student_2))[0];
+  }
+
+  return pairings;
+
+}
 
 router.get('/', parser(url), async (ctx, next) => {
 
@@ -24,8 +43,57 @@ router.get('/', parser(url), async (ctx, next) => {
   // 404 if there is no matching student
   if (!student) return;
 
-  // TODO get the matching pairings
-  ctx.response.body = student;
+  const required = Math.min(
+    moment().weeks() - moment(start).weeks(),
+    weeks,
+  );
+
+  let pairings = await knex.select()
+    .from('pairing')
+    .where('class', classId)
+    .where(function() {
+      this.where('student_1', student.id)
+        .orWhere('student_2', student.id);
+    })
+    .orderBy('week', 'asc');
+
+  if (pairings.length === required) {
+    return ctx.response.body = await attachStudents(pairings);
+  }
+
+  for (let week = pairings.length + 1; week <= required; week++) {
+
+    // TODO use knex for this
+    const result = await knex.raw(`
+      select b.id
+      from student a
+      inner join student b on a.id != b.id
+      where a.id = '${student.id}'
+      and not exists (
+        select student_2
+        from pairing
+        where (student_1 = b.id OR student_2 = b.id AND week = 1)
+          or (
+            student_1 in (a.id, b.id) AND student_2 in (a.id, b.id)
+          )
+      )
+      limit 1;
+     `);
+
+    const pairing = {
+      class: classId,
+      week,
+      student_1: student.id,
+      student_2: result.rows[0].id,
+      image: null,
+    };
+
+    await knex('pairing').insert(pairing);
+    pairings.push(pairing);
+  }
+
+  ctx.response.body = await attachStudents(pairings);
+
 });
 
 module.exports = router;
